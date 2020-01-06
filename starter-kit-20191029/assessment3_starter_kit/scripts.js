1deed35653bca19b42d3ddf227732b74b47d76ad
Alicia Martin <anmartin114@gmail.com>
2020-01-06 13:47:19 -0500

    initial commit

---
+++ starter-kit-20191029/assessment3_starter_kit/scripts.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let cells = document.querySelectorAll(".cell");
+cells[Math.floor(Math.random() * 16)].id = "it";
+
+document.querySelector("section").addEventListener("click", clicked);
+
+function clicked(event) {
+  if (event.target.classList.contains("cell"))
+    if (event.target.id === "it") {
+      event.target.classList.toggle("red");
+      for (const cell of cells) {
+        if (cell.classList.contains("green")) {
+          cell.classList.toggle("yellow");
+        }
+      }
+      document.querySelector("section").removeEventListener("click", clicked);
+    } else {
+      event.target.classList.toggle("green");
+    }
+}
+
+
